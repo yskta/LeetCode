d5a5b890523d9c9aa0624298cc264aa69330b534
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-12-10 14:41:42 +0900

    push

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -5,10 +5,31 @@
  */
 
 // @lc code=start
+#include <stack>
+
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char> stk;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                stk.push(c);
+            } else {
+                if (stk.empty()) {
+                    return false;  // 開き括弧がないのに閉じ括弧が現れた場合
+                }
+                char openBracket = stk.top();// 一番上の要素を取り出す
+                stk.pop();// 一番上の要素を削除する
+
+                if ((c == ')' && openBracket != '(') ||
+                    (c == ']' && openBracket != '[') ||
+                    (c == '}' && openBracket != '{')) {
+                    return false;  // 対応する開き括弧がない場合
+                }
+            }
+        }
+        return stk.empty();  // スタックが空であれば有効な括弧の組み合わせ
     }
 };
 // @lc code=end
