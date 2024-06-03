120ab9e2c2f3172e7c4bdce53a75465828c087ad
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-06-03 12:47:49 +0900

    push

--- 119.pascals-triangle-ii.cpp
+++ 119.pascals-triangle-ii.cpp
@@ -8,7 +8,17 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        
+        vector<int> result(rowIndex + 1, 1); // 事前確保と初期化
+        if (rowIndex == 0) {
+            return result;
+        }
+        for (int i = 1; i <= rowIndex; ++i) {
+            result.push_back(1);
+            for (int j = i - 1; j > 0; --j) {
+                result[j] += result[j - 1];
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
